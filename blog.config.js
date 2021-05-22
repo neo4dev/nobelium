8e36743c62687b41e695371481fa51c9a35d1ed9
neo4dev <neo4dev@qq.com>
2021-05-22 12:56:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: '增长浩克',
+  author: 'Hawk',
+  email: 'neo4pm@qq.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
